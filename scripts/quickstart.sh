8498603a8d4873ffb31551aba1b11a47730dea58
Budi Darmawan <vbudi@us.ibm.com>
2022-02-22 08:14:20 -0600

    update typo on quickstart

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -40,7 +40,7 @@ popd () {
     command popd "$@" > /dev/null
 }
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+QS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 [[ -n "${DEBUG:-}" ]] && set -x
 
@@ -131,7 +131,7 @@ run_bootstrap() {
     echo -e "${WHITE}Invoking bootstrap script${NC}"
     curl -sfL https://raw.githubusercontent.com/cloud-native-toolkit/multi-tenancy-gitops/master/scripts/bootstrap.sh | DEBUG="" GIT_ORG=${GIT_ORG} OUTPUT_DIR=${OUTPUT_DIR} IBM_ENTITLEMENT_KEY=${IBM_ENTITLEMENT_KEY} CP_EXAMPLE=${CP_EXAMPLE} bash
     pushd ${OUTPUT_DIR}/gitops-0-bootstrap
-    source ./script/set-git-source.sh
+    source ./scripts/set-git-source.sh
     git add .
     git commit -m "setting git source for argoCD for $GIT_ORG"
     git push 
